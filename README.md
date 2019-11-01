4d85fef63a7ad48c839d0195e5542d426e12d352
Michiel <michiel@herosolutions.be>
2019-11-01 15:14:16 +0100

    Adds readme, adds configurable options to delegate script

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-TODO write me
+# Cantaloupe delegate script
+
+This project contains a Cantaloupe delegate script that can be used in conjunction with a local [Imagehub](https://github.com/kmska/ImageHub) and [ResourceSpace](https://www.resourcespace.com/get) (preferably with the [RS_ptif](https://github.com/kmska/RS_ptif) plugin installed). The code in this delegate script is specifically aimed toward authentication of a user through a locally installed third-party SAML service, for example the [Imagehub](https://github.com/kmska/ImageHub).
+
+
+## Requirements
+
+This project requires the following dependencies:
+* [ResourceSpace](https://www.resourcespace.com/get) >= 9.1 with the [RS_ptif](https://github.com/kmska/RS_ptif) plugin installed
+* A local [Imagehub](https://github.com/kmska/ImageHub) installation
+
+# Usage
+
+Cantaloupe comes bundled with a sample delegate scrips ``delegates.rb.sample``, containing several delegate methods. The delegates.rb script in this project overrides the ``authenticate`` method.
+
+In order to use the authentication in this project, you need to copy-paste the complete ``delegates.rb`` into your cantaloupe installation folder or copy-paste the imports. If you are already using the delegate script for other purposes, then you can copy-paste the imports, the ``@@private_keyword``, ``@@authcheck_url`` and ``@@authenticator_url`` values and the ``authenticate`` method from the delegates.rb file in this project into your delegate script.
+
+You also need to set ``delegate_script.enabled = true`` and ``delegate_script.pathname = delegates.rb`` (or whatever your own delegate script is called) in cantaloupe.properties.
--- delegates.rb
+++ delegates.rb
@@ -2,58 +2,31 @@ require 'openssl'
 require 'net/http'
 require 'uri'
 
-##
+#
 # Ruby delegate script for authentication against a SAML service
-#
-# The application will create an instance of this class early in the request
-# cycle and dispose of it at the end of the request cycle. Instances don't need
-# to be thread-safe, but sharing information across instances (requests)
-# **does** need to be done thread-safely.
-#
-# This version of the script works with Cantaloupe version 4, and not earlier
-# versions. Likewise, earlier versions of the script are not compatible with
-# Cantaloupe 4.
 #
 class CustomDelegate
 
+  # Configuration of the authorize() method
+
+  # The keyword that determines if a user needs to authenticate in order to access a file
+  @@private_keyword = 'private'
+
+  # The URL that will be called to check if this user is already authenticated
+  # All request cookies will be passed to this URL
+  # If this URL returns a 200 response code, serve the image
+  # If it returns a 302 response code, the user needs to authenticate
+  # Any other response codes than 200 or 302 will result in the user not being allowed to access the image
+  @@authcheck_url = 'https://imagehub.kmska.be/imagehub/authcheck'
+
+  # URL where to redirect the user if they are not yet authenticated
+  # This URL is expected to redirect back to here if authentication was successful
+  # The current request_uri will be appended to this URL so it knows where to redirect to
+  @@authenticator_url = 'https://imagehub.kmska.be/imagehub/authenticate?url='
+
+
   ##
-  # Attribute for the request context, which is a hash containing information
-  # about the current request.
-  #
-  # This attribute will be set by the server before any other methods are
-  # called. Methods can access its keys like:
-  #
-  # ```
-  # identifier = context['identifier']
-  # ```
-  #
-  # The hash will contain the following keys in response to all requests:
-  #
-  # * `client_ip`        [String] Client IP address.
-  # * `cookies`          [Hash<String,String>] Hash of cookie name-value pairs.
-  # * `identifier`       [String] Image identifier.
-  # * `request_headers`  [Hash<String,String>] Hash of header name-value pairs.
-  # * `request_uri`      [String] Public request URI.
-  # * `scale_constraint` [Array<Integer>] Two-element array with scale
-  #                      constraint numerator at position 0 and denominator at
-  #                      position 1.
-  #
-  # It will contain the following additional string keys in response to image
-  # requests:
-  #
-  # * `full_size`      [Hash<String,Integer>] Hash with `width` and `height`
-  #                    keys corresponding to the pixel dimensions of the
-  #                    source image.
-  # * `operations`     [Array<Hash<String,Object>>] Array of operations in
-  #                    order of application. Only operations that are not
-  #                    no-ops will be included. Every hash contains a `class`
-  #                    key corresponding to the operation class name, which
-  #                    will be one of the `e.i.l.c.operation.Operation`
-  #                    implementations.
-  # * `output_format`  [String] Output format media (MIME) type.
-  # * `resulting_size` [Hash<String,Integer>] Hash with `width` and `height`
-  #                    keys corresponding to the pixel dimensions of the
-  #                    resulting image after all operations have been applied.
+  # Attribute for the request context, which is a hash containing information about the current request.
   #
   # @return [Hash] Request context.
   #
@@ -62,27 +35,6 @@ class CustomDelegate
   ##
   # Returns authorization status for the current request. Will be called upon
   # all requests to all public endpoints.
-  #
-  # Implementations should assume that the underlying resource is available,
-  # and not try to check for it.
-  #
-  # Possible return values:
-  #
-  # 1. Boolean true/false, indicating whether the request is fully authorized
-  #    or not. If false, the client will receive a 403 Forbidden response.
-  # 2. Hash with a `status_code` key.
-  #     a. If it corresponds to an integer from 200-299, the request is
-  #        authorized.
-  #     b. If it corresponds to an integer from 300-399:
-  #         i. If the hash also contains a `location` key corresponding to a
-  #            URI string, the request will be redirected to that URI using
-  #            that code.
-  #         ii. If the hash also contains `scale_numerator` and
-  #            `scale_denominator` keys, the request will be
-  #            redirected using that code to a virtual reduced-scale version of
-  #            the source image.
-  #     c. If it corresponds to 401, the hash must include a `challenge` key
-  #        corresponding to a WWW-Authenticate header value.
   #
   # @param options [Hash] Empty hash.
   # @return [Boolean,Hash<String,Object>] See above.
@@ -95,7 +47,7 @@ class CustomDelegate
     host_index = request_uri.index(uri.host) + uri.host.length
     path = request_uri[host_index..request_uri.length - 1]
 
-    private_index = path.index('private')
+    private_index = path.index(@@private_keyword)
     puts private_index
     if private_index.nil?
       true
@@ -104,7 +56,7 @@ class CustomDelegate
       # See https://github.com/kmska/Imagehub
 
       # First, check if the user is already authenticated
-      uri = URI('https://imagehub.kmska.be/imagehub/authcheck')
+      uri = URI(@@authcheck_url)
       req = Net::HTTP::Get.new(uri)
       response = Net::HTTP.start(
               uri.host, uri.port,
@@ -119,7 +71,7 @@ class CustomDelegate
       if response.code == '302'
         {
           'status_code' => 302,
-          'location' => 'https://imagehub.kmska.be/imagehub/authenticate?url=' + URI::encode(context['request_uri'])
+          'location' => @@authenticator_url + URI::encode(context['request_uri'])
         }
       # If the user is already authenticated, allow access to the image
       elsif response.code == '200'
